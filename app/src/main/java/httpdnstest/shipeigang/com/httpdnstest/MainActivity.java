cd89f1f76b13051a972170c4a25b9cd1277970d1
sipg <597382457@qq.com>
2019-03-21 11:10:18 +0800

    init project

---
+++ app/src/main/java/httpdnstest/shipeigang/com/httpdnstest/MainActivity.java
@@ -0,0 +1,13 @@
+package httpdnstest.shipeigang.com.httpdnstest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
